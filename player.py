6ce674159ee65ab0aa5d9a385ae3d656613af3d0
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-11-16 18:29:26 -0300

    add minor documentation

--- player.py
+++ player.py
@@ -13,12 +13,20 @@ class Player:
     self.lose = False
   def receive_money(self, ammount):
     self.money += ammount
+  '''
+  This function subracts the player's money.
+  If a debt is verified, the flag lose sets True.
+  '''
   def pay_money(self, ammount):
     self.money -= ammount
     if (self.money < 0):
       self.lose = True
   def buy_propoerty(self, property):
     self.pay_money(property.sell_cost)
+  '''
+  This function performs the transaction of paying a rent 
+    from this player to a player owner of a given property
+  '''
   def pay_rent(self, property):
     self.pay_money(property.rent_cost)
     property.owner.receive_money(property.rent_cost)
