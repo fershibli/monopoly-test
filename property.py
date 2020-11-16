5dbbe4692fc69795a96bca2b15917271347d02d0
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-11-16 13:17:23 -0300

    add expropriate and appropriate actions to Property

--- property.py
+++ property.py
@@ -1,6 +1,17 @@
+'''
+This package only contains Player class
+'''
+
+'''
+This class manages all commom actions and attributes of a single Property of the GameTable class.
+'''
 class Property:
   def __init__(self, name, sell_cost, rent_cost):
     self.name =  name
     self.sell_cost = sell_cost
     self.rent_cost = rent_cost
     self.owner = None
+  def expropriate_property(self):
+    self.owner = None
+  def appropriate_property(self, player):
+    self.owner = player
