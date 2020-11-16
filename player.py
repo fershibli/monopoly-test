94e8b2f8379a3c37ee3818511d8ea26e59f2b6db
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-11-16 13:19:17 -0300

    add position and money actions to Player

--- player.py
+++ player.py
@@ -1,11 +1,26 @@
+'''
+This package only contains Player class
+'''
+
+'''
+This class manages all commom actions and attributes of a single Player of the GameTable Class
+'''
 class Player:
-  def __init__(self, name, money):
+  def __init__(self, name, money = 0):
     self.name = name
     self.money = money
+    self.position = 0
     self.lose = False
+  def receive_money(self, ammount):
+    self.money += ammount
+  def pay_money(self, ammount):
+    self.money -= ammount
+    if (self.money < 0):
+      self.lose = True
   def buy_propoerty(self, property):
-    self.money -= property.sell_cost
-    property.owner = self
+    self.pay_money(property.sell_cost)
   def pay_rent(self, property):
-    self.money -= property.rent_cost
-    property.owner.money += property.rent_cost
+    self.pay_money(property.rent_cost)
+    property.owner.receive_money(property.rent_cost)
+  def move_position(self, movement):
+    self.position += movement
