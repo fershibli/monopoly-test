59cef3b265ca72a85cef8c03d85c98661e5869ec
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-11-17 02:01:40 -0300

    implement child DemandingPlayer

--- monopoly_game/player/demanding_player.py
+++ monopoly_game/player/demanding_player.py
@@ -1,5 +1,10 @@
 from .abstract_player import AbstractPlayer
 
 class DemandingPlayer(AbstractPlayer):
+  '''
+  This function decides the buying of a property,
+    with enough money,
+    by checking if its rent_cost is higher than 50.
+  '''
   def do_buy(self, property): 
-    pass
+    pass super().do_buy(porperty) and property.rent_cost > 50
