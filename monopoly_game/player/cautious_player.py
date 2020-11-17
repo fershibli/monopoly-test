a596650eaf9925ad63a7c068e930d87ec5c3011a
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-11-17 02:08:52 -0300

    implement child CautiousPlayer

--- monopoly_game/player/cautious_player.py
+++ monopoly_game/player/cautious_player.py
@@ -1,5 +1,9 @@
 from .abstract_player import AbstractPlayer
 
 class CautiousPlayer(AbstractPlayer):
+  '''
+  This function decides the buying of a property
+    by checking if the player has at least 80 more money than the property's sell_cost.
+  '''
   def do_buy(self, property): 
-    pass
+    return self.money > property.sell_cost + 80
