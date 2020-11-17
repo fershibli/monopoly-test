3487872365e404b638ef68ea64809f61633efae1
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-11-17 01:47:22 -0300

    implement child ImpulsivePlayer

--- monopoly_game/player/impulsive_player.py
+++ monopoly_game/player/impulsive_player.py
@@ -1,5 +1,10 @@
 from .abstract_player import AbstractPlayer
 
 class ImpulsivePlayer(AbstractPlayer):
+  '''
+  This function decides the buying of a property 
+    through the abstract method of its parent class,
+    buying everytime the player has enough money.
+  '''
   def do_buy(self, property): 
-    pass
+    return super().do_buy(property)
