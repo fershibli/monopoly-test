c079f64b3b11a027b4b6f907fe8f47684db08406
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2020-11-17 01:55:28 -0300

    implement child RandomPlayer

--- monopoly_game/player/random_player.py
+++ monopoly_game/player/random_player.py
@@ -1,5 +1,14 @@
 from .abstract_player import AbstractPlayer
+from random import getrandbits
 
 class RandomPlayer(AbstractPlayer):
-  def do_buy(self, property): 
-    pass
+  '''
+  This function decides the buying of a property,
+    with enough money,
+    equally and randomly between 0 and 1 as bits, 
+    then transforms it into boolean before returning it.
+
+  That's the best approach for performance so far.
+  '''
+  def do_buy(self, property):
+    return super().do_buy(property) and bool(getrandbits(1))
